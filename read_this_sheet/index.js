eb3cfa6ddfdfe5e5a699429c26f2d2bb338c52a4
fellainthewagon <fellainthewagon@gmail.com>
2021-10-08 14:06:16 +0300

    trycatch added

--- read_this_sheet/index.js
+++ read_this_sheet/index.js
@@ -2,24 +2,28 @@ const { promises: fs, createReadStream } = require("fs");
 const path = require("path");
 
 async function readableStream(folder) {
-  const file = await fs.readdir(folder);
-  if (!file.length) throw new Error("Folder is empty");
+  try {
+    const file = await fs.readdir(folder);
+    if (!file.length) throw new Error("Folder is empty");
 
-  const stream = createReadStream(
-    path.join(__dirname, folder, file[0]),
-    "utf8"
-  );
+    const stream = createReadStream(
+      path.join(__dirname, folder, file[0]),
+      "utf8"
+    );
 
-  let i = 0;
-  stream.on("data", (chunk) => {
-    console.log(`Chunk ${i}: <${chunk.slice(0, 15)}>...`);
+    let i = 0;
+    stream.on("data", (chunk) => {
+      console.log(`Chunk ${i}: <${chunk.slice(0, 15)}>...`);
 
-    i++;
-  });
+      i++;
+    });
 
-  stream.on("end", () => {
-    console.log("The sheet has been fully read!");
-  });
+    stream.on("end", () => {
+      console.log("The sheet has been fully read!");
+    });
+  } catch (error) {
+    console.error("Something went wrong...\n", error);
+  }
 }
 
 readableStream("source");
